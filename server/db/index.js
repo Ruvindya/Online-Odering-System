60b47bf0acad3961be99c33368d472fe7f343c69
Ruvindya <2019e117@eng.jfn.ac.lk>
2023-09-12 20:32:27 +0530

    Database connected success

--- server/db/index.js
+++ server/db/index.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 mongoose
-    .connect('mongodb://localhost:27017/food-ordering',{useNewUrlParser: true})
+    .connect('mongodb+srv://food-ordering:food-ordering@cluster0.p61igz7.mongodb.net/?retryWrites=true&w=majority',{useNewUrlParser: true})
     .catch(e => {
         console.error('Connection error', e.message)
     })
